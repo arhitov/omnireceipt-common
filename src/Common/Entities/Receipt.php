acf9e0428bd9c3663515825be15450522f422229
clgsru <clgsru@gmail.com>
2024-05-08 17:40:42 +0300

    Removed PHPDoc for getId, because this interfered with static analysis.

--- src/Common/Entities/Receipt.php
+++ src/Common/Entities/Receipt.php
@@ -16,10 +16,6 @@ use Omnireceipt\Common\Contracts\ReceiptItemInterface;
 use Omnireceipt\Common\Supports\ParametersTrait;
 
 /**
- * @method string getId()
- * @method string getIdOrNull()
- * @method self setId(string $value)
- *
  * @method string getType()
  * @method string getTypeOrNull()
  * @method self setType(string $value)
